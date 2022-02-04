a775a19779340af72944006448d9831713a3a571
L1ghtboro <tiptemka@gmail.com>
2022-02-04 14:51:17 +0200

    Creating C++ Project and adding OpenGL Libraries

---
+++ 3DModelViewer/Main.cpp
@@ -0,0 +1,12 @@
+/*Note: Up here you can store only main function and
+links to other header file. No more functions here*/
+
+//Start Implementing Section:
+#include "Modules.h"
+//End Implementing Section
+
+
+int main(void) {
+
+	return 0;
+}
